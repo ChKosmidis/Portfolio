f0d17f00b8f96727a02b56f4cc3c4407cff5613a
ChKosmidis <kosmidis.hse@gmail.com>
2025-07-30 05:06:35 +0200

    v.1.7

--- .next/static/chunks/src_app_page_tsx_d846e3da._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_a4c1bff3._.js",
-  "static/chunks/src_components_05255038._.js"
-],
-    source: "dynamic"
-});
